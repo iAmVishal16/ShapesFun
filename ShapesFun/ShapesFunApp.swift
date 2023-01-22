d22ceff24e79fed85d2a99f95e8412d238abcbc0
Vishal Paliwal <paliwalvishal16@gmail.com>
2023-01-22 20:18:57 +0530

    Initial Commit

---
+++ ShapesFun/ShapesFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesFunApp.swift
+//  ShapesFun
+//
+//  Created by Vishal Paliwal on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ShapesFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
